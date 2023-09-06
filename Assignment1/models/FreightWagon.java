49df9ebb92292a87aa54e9d7b0c68d82c3a974cc
Joey van der Poel <Joey.van.der.poel@gmail.com>
2023-09-06 15:57:00 +0200

    A1 eerste opdracht af.

---
+++ Assignment1/models/FreightWagon.java
@@ -0,0 +1,19 @@
+package models;
+// TODO
+public class FreightWagon {
+
+    public int maxWeight;
+
+    public void setMaxWeight(int maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    public FreightWagon(int wagonId, int maxWeight) {
+        // TODO
+    }
+
+    public int getMaxWeight() {
+        // TODO
+        return 0;
+    }
+}
---
+++ Assignment1/models/PassengerWagon.java
@@ -0,0 +1,20 @@
+package models;
+// TODO
+public class PassengerWagon {
+
+    public int numberOfSeats;
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public PassengerWagon(int wagonId, int numberOfSeats) {
+        // TODO
+    }
+
+    public int getNumberOfSeats() {
+        // TODO
+
+        return 0;
+    }
+}
