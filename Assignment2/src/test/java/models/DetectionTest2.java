fbe0d33afaa125526aeb96064b09a65627ad5318
Kerim Yildizeli <kerim.yildizeli@hva.nl>
2023-10-12 14:49:01 +0200

    DetectionTest2: done

--- Assignment2/src/test/java/models/DetectionTest2.java
+++ Assignment2/src/test/java/models/DetectionTest2.java
@@ -9,9 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DetectionTest2 {
 
@@ -21,15 +19,15 @@ public class DetectionTest2 {
     @BeforeEach
     public void setup() {
         Locale.setDefault(Locale.ENGLISH);
-        scoda = new Car("1-AAA-02", 6, Car.CarType.Car, Car.FuelType.Gasoline, LocalDate.of(2014,1,31));
-        audi = new Car("AA-11-BB", 4, Car.CarType.Car, Car.FuelType.Diesel, LocalDate.of(1998,1,31));
-        mercedes = new Car("VV-11-BB", 4, Car.CarType.Van, Car.FuelType.Diesel, LocalDate.of(1998,1,31));
-        bmw = new Car("A-123-BB", 4, Car.CarType.Car, Car.FuelType.Gasoline, LocalDate.of(2019,1,31));
-        icova = new Car("1-TTT-99", 5, Car.CarType.Truck, Car.FuelType.Lpg, LocalDate.of(2011,1,31));
-        volvo1 = new Car("1-TTT-01", 5, Car.CarType.Truck, Car.FuelType.Diesel, LocalDate.of(2009,1,31));
-        volvo2 = new Car("1-TTT-02", 6, Car.CarType.Truck, Car.FuelType.Diesel, LocalDate.of(2011,1,31));
-        daf1 = new Car("1-CCC-01", 5, Car.CarType.Coach, Car.FuelType.Diesel, LocalDate.of(2009,1,31));
-        daf2 = new Car("1-CCC-02", 6, Car.CarType.Coach, Car.FuelType.Diesel, LocalDate.of(2011,1,31));
+        scoda = new Car("1-AAA-02", 6, Car.CarType.Car, Car.FuelType.Gasoline, LocalDate.of(2014, 1, 31));
+        audi = new Car("AA-11-BB", 4, Car.CarType.Car, Car.FuelType.Diesel, LocalDate.of(1998, 1, 31));
+        mercedes = new Car("VV-11-BB", 4, Car.CarType.Van, Car.FuelType.Diesel, LocalDate.of(1998, 1, 31));
+        bmw = new Car("A-123-BB", 4, Car.CarType.Car, Car.FuelType.Gasoline, LocalDate.of(2019, 1, 31));
+        icova = new Car("1-TTT-99", 5, Car.CarType.Truck, Car.FuelType.Lpg, LocalDate.of(2011, 1, 31));
+        volvo1 = new Car("1-TTT-01", 5, Car.CarType.Truck, Car.FuelType.Diesel, LocalDate.of(2009, 1, 31));
+        volvo2 = new Car("1-TTT-02", 6, Car.CarType.Truck, Car.FuelType.Diesel, LocalDate.of(2011, 1, 31));
+        daf1 = new Car("1-CCC-01", 5, Car.CarType.Coach, Car.FuelType.Diesel, LocalDate.of(2009, 1, 31));
+        daf2 = new Car("1-CCC-02", 6, Car.CarType.Coach, Car.FuelType.Diesel, LocalDate.of(2011, 1, 31));
         kamaz = new Car("1-AAAA-0000");
         cars = new ArrayList<>(List.of(scoda, audi, mercedes, bmw, icova, volvo1, volvo2, daf1, daf2, kamaz));
     }
@@ -37,11 +35,12 @@ public class DetectionTest2 {
     @Test
     public void testValidatePurpleViolation() {
         // Create a Detection instance that should result in a Violation
-        Detection detection1 = new Detection(volvo2, "Amsterdam", LocalDateTime.of(2022, 10, 1, 12, 11, 10));
+        Detection detection1 = new Detection(volvo1, "Amsterdam", LocalDateTime.of(2022, 10, 1, 12, 11, 10));
 
         // Validate against purple conditions
         Violation violation1 = detection1.validatePurple();
-        assertSame(volvo2, violation1.getCar());
+        assertNotNull(violation1); // Assert that a Violation should be returned
+        assertEquals(volvo1, violation1.getCar());
         assertEquals("Amsterdam", violation1.getCity());
     }
 
