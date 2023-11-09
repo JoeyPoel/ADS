f65ef0aa2fb139a8212e2d65ed171f097bf4b7b4
Kerim Yildizeli <kerim.yildizeli@hva.nl>
2023-11-09 16:33:18 +0100

    Ongoing with Big-O

--- Assignment3/src/main/java/SpotifyChartsMain.java
+++ Assignment3/src/main/java/SpotifyChartsMain.java
@@ -16,92 +16,67 @@ public class SpotifyChartsMain {
         if (k < 50000000) {
             long startTime;
             long endTime;
-            boolean alreadyPrinted = false;
 
 
             Sorter<Song> sorter = new SongSorter();
-            Sorter<Song> sorter2 = new SongSorter();
-            Sorter<Song> sorter3 = new SongSorter();
-            Sorter<Song> sorter4 = new SongSorter();
-
-            for (int i = 1; i <= 10; i++) {
-                long totalTime = 0;
-
-                for (int j = 0; j < 10; j++) {
 
 
-                ChartsCalculator chartsCalculator = new ChartsCalculator(i * j);
-
-                List<Song> songs = chartsCalculator.registerStreamedSongs(k);
-
-
-                // Test 1
-                startTime = System.currentTimeMillis();
-                sorter.selInsBubSort(songs, Song::compareByHighestStreamsCountTotal);
-                System.gc();
-                endTime = System.currentTimeMillis();
-                long timeResult = endTime - startTime;
-                totalTime += timeResult;
-
-                // Test 2
-                startTime = System.currentTimeMillis();
-                sorter2.quickSort(songs, Song::compareByHighestStreamsCountTotal);
-                System.gc();
-                endTime = System.currentTimeMillis();
-                long timeResult2 = endTime - startTime;
-                totalTime += timeResult2;
+            for (int algorithm = 1; algorithm <= 4; algorithm++) {
+                boolean algorithmExceededTime = false;
+                long totalExecutionTime = 0;
 
 
-                    // Test 3
-                startTime = System.currentTimeMillis();
-                sorter3.topsHeapSort(5, songs, Song::compareByHighestStreamsCountTotal);
-                System.gc();
-                endTime = System.currentTimeMillis();
-                long timeResult3 = endTime - startTime;
-                totalTime += timeResult3;
+                for (int i = 0; i < 10; i++) {
 
 
-                    // Test 4
-                startTime = System.currentTimeMillis();
-                sorter4.selInsBubSort(songs, Song::compareByHighestStreamsCountTotal);
-                System.gc();
-                endTime = System.currentTimeMillis();
-                long timeResult4 = endTime - startTime;
-                totalTime += timeResult4;
+                    ChartsCalculator chartsCalculator = new ChartsCalculator(algorithm * i);
+
+                    List<Song> songs = chartsCalculator.registerStreamedSongs(k);
+
+                    startTime = System.currentTimeMillis();
+                    switch (algorithm) {
+                        case 1:
+                            sorter.selInsBubSort(songs, Song::compareByHighestStreamsCountTotal);
+                            break;
+                        case 2:
+                            sorter.quickSort(songs, Song::compareByHighestStreamsCountTotal);
+                            break;
+                        case 3:
+                            sorter.topsHeapSort(5, songs, Song::compareByHighestStreamsCountTotal);
+                            break;
+                        case 4:
+                            sorter.selInsBubSort(songs, Song::compareByHighestStreamsCountTotal);
+                            break;
+                    }
+
+                    System.gc();
+
+                    endTime = System.currentTimeMillis();
+                    long executionTime = endTime - startTime;
+
+                    totalExecutionTime += executionTime;
+
+                    if (executionTime > 20000) {
+                        algorithmExceededTime = true;
+                        break;
+                    }
+
+                    System.out.println("-------------------------------------------------------");
+                    System.out.println("\nAlgorithm " + algorithm + ":");
+                     System.out.println("Run " + i + ": " + executionTime + "ms");
+
+                    if (algorithmExceededTime) {
+                        System.out.println("Algorithm " + algorithm + " exceeded 20 seconds");
+                        System.out.println(totalExecutionTime);
+                    }
+
+                    k = k * 2;
 
 
-                if (timeResult > 20000 && timeResult2 > 20000 && timeResult3 > 20000 && timeResult4 > 20000) {
-                    System.out.println("Execution time exceeded 20 seconds");
-                    return 0;
                 }
             }
-                if (!alreadyPrinted) {
-
-
-                    System.out.println(
-                            "\nAverage execution time for Algorithm 1 " + totalTime / 10 + "ms"
-                    );
-                    System.out.println(
-                            "\nAverage execution time for Algorithm 2 " + totalTime / 10 + "ms"
-                    );
-                    System.out.println(
-                            "\nAverage execution time for Algorithm 3 " + totalTime / 10 + "ms"
-                    );
-                    System.out.println(
-                            "\nAverage execution time for Algorithm 4 " + totalTime / 10 + "ms"
-                    );
-
-                    alreadyPrinted = true;
-                }
-
-
-                return sum(k * 2);
-            }
-        } else {
-            return 0;
         }
         return 0;
+
     }
-
-
 }
